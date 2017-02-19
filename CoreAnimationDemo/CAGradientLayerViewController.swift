9f8f3cf587aa936770df48a697de6b163d8bf0d1
hao <740697612@qq.com>
2017-02-20 00:54:02 +0800

    Added CAScrollLayer, CATiledLayer, CAReplicatorLayer and TileCutter

--- CoreAnimationDemo/CAGradientLayerViewController.swift
+++ CoreAnimationDemo/CAGradientLayerViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class CAGradientLayerViewController: UIViewController {
 
-    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var containerView: VTGradientView!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -18,17 +18,25 @@ class CAGradientLayerViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        // 创建渐变图层
-        let layer = CAGradientLayer()
-        // startPoint / endPoint 设置渐变的起始和结束点，用于确定渐变的方向
-        // 左上角为{0, 0}, 右下角为{0, 0}
-        layer.startPoint = CGPoint(x: 0, y: 0)
-        layer.endPoint = CGPoint(x: 1, y: 1)
+//        // 创建渐变图层
+//        let layer = CAGradientLayer()
+//        // startPoint / endPoint 设置渐变的起始和结束点，用于确定渐变的方向
+//        // 左上角为{0, 0}, 右下角为{0, 0}
+//        layer.startPoint = CGPoint(x: 0, y: 0)
+//        layer.endPoint = CGPoint(x: 1, y: 1)
+//        
+//        // 渐变颜色
+//        layer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+//        
+//        layer.frame = containerView.bounds
+//        
+//        containerView.layer.addSublayer(layer)
         
-        layer.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        containerView.startPoint = CGPoint(x: 0, y: 0)
+        containerView.endPoint = CGPoint(x: 1, y: 1)
         
-        layer.frame = containerView.bounds
+        containerView.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor, UIColor.green.cgColor]
         
-        containerView.layer.addSublayer(layer)
+        containerView.locations = [.init(value: 0.0), .init(value: 0.25), .init(value: 0.5)]
     }
 }
---
+++ CoreAnimationDemo/CAReplicatorLayerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  CAReplicatorLayerViewController.swift
+//  CoreAnimationDemo
+//
+//  Created by hao on 2017/2/19.
+//  Copyright © 2017年 Vito. All rights reserved.
+//
+
+import UIKit
+
+class CAReplicatorLayerViewController: UIViewController {
+    @IBOutlet weak var containerView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 创建CAReplicatorLayer
+        let replicatorLayer = CAReplicatorLayer()
+        replicatorLayer.frame = containerView.bounds
+        containerView.layer.addSublayer(replicatorLayer)
+        // 设置复制个数
+        replicatorLayer.instanceCount = 10
+        
+        var transform = CATransform3DIdentity
+        transform = CATransform3DTranslate(transform, 0, 200, 0)
+        transform = CATransform3DRotate(transform, CGFloat(M_PI / 5.0), 0, 0, 1)
+        transform = CATransform3DTranslate(transform, 0, -200, 0)
+        replicatorLayer.instanceTransform = transform
+        
+        replicatorLayer.instanceBlueOffset = -0.1
+        replicatorLayer.instanceGreenOffset = -0.1
+        
+        // 创建一个子layer 并添加到replicator layer中
+        let layer = CALayer()
+        layer.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        layer.backgroundColor = UIColor.white.cgColor
+        replicatorLayer.addSublayer(layer)
+    }
+
+}
